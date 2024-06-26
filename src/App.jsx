bb5e4e3f91a96c9b085dc6480c69bcb6d094ec76
Niraj Parmar <nirajprmr1608@gmail.com>
2024-06-26 14:35:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Todo from "./components/Todo"
+
+export const App = () => {
+  return (
+    <div>
+      <Todo />
+    </div>                                                                                                    
+  )
+}
+
+export default App
